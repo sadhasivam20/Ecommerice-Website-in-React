2bc8a9d71bfc0b45eb826962034b88554f205c19
sadhasivam20 <sadha2040@gmail.com>
2023-01-21 19:40:41 +0530

    WEBCODE 2

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>9003504790 </label>
+            <i className='fa fa-envelope'></i>
+            <label> sadha2040@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
